f79d64f551ec4bf60a2ed500b5ed98873057f2fa
amapacheco <alejandropacheco@hotmail.com>
2018-10-12 16:31:19 -0300

    first commit

---
+++ ApiJam.md
@@ -0,0 +1,3 @@
+# Setup Files for API Jam
+
+We DO remcomend use shell avialable in *any* cloud Provider
---
+++ get-codebase.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir apijam
+cd apijam
+
+git clone https://github.com/apigeekdemos/apigee-s1p-labs
+git clone https://github.com/apigeekdemos/cloud-foundry-apigee.git
+git clone https://github.com/apigeekdemos/microgateway.git
